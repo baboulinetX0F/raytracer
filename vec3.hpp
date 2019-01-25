e25241ac005f26ca35b00431d04fc1f2f9c30ef4
baboulinet_ <vergilotaku@live.fr>
2019-01-25 18:42:50 +0100

    cleaned vec3 / fixed operator problem

--- vec3.hpp
+++ vec3.hpp
@@ -1,10 +1,11 @@
 #include <math.h>
+#include <stdlib.h>
 #include <iostream>
 
 class vec3
 {
 public:
-    vec3() {e[0] = e[1] = e[2] = 0.0;}
+    vec3() { }
     vec3(float x, float y, float z) {e[0]=x; e[1]=y; e[2]=z; }
     inline float x() const { return e[0]; }
     inline float y() const { return e[1]; }
@@ -17,16 +18,12 @@ public:
     inline float operator[](int i) const { return e[i]; }
     
     inline vec3& operator+=(const vec3& v2);
-    inline vec3& operator+(const vec3& v2);
-    inline vec3& operator*(const vec3& v2);
     inline vec3& operator*=(const vec3& v2);
-    inline vec3& operator/(const vec3& v2);
     inline vec3& operator/=(const vec3& v2);
     inline vec3& operator*(const float f);
     inline vec3& operator/(const float f);
     inline vec3& operator*=(const float f);
     inline vec3& operator/=(const float f);
-    inline vec3& operator-(const vec3& v2);
     inline vec3& operator-=(const vec3& v2);
 
     inline float length() const { return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
@@ -37,41 +34,25 @@ public:
 
 inline vec3& vec3::operator+=(const vec3& v2)
 {
-    e[0]+=v2.x();
-    e[1]+=v2.y();
-    e[2]+=v2.z();
-    return *this;
-}
-
-inline vec3& vec3::operator+(const vec3& v2)
-{
-    e[0]+=v2.x();
-    e[1]+=v2.y();
-    e[2]+=v2.z();
+    e[0]+=v2.e[0];
+    e[1]+=v2.e[1];
+    e[2]+=v2.e[2];
     return *this;
 }
 
 inline vec3& vec3::operator*=(const vec3& v2)
 {
-    e[0]*=v2.x();
-    e[1]*=v2.y();
-    e[2]*=v2.z();
-    return *this;
-}
-
-inline vec3& vec3::operator*(const vec3& v2)
-{
-    e[0]*=v2.x();
-    e[1]*=v2.y();
-    e[2]*=v2.z();
+    e[0]*=v2.e[0];
+    e[1]*=v2.e[1];
+    e[2]*=v2.e[2];
     return *this;
 }
 
 inline vec3& vec3::operator-=(const vec3& v2)
 {
-    e[0]-=v2.x();
-    e[1]-=v2.y();
-    e[2]-=v2.z();
+    e[0]-=v2.e[0];
+    e[1]-=v2.e[1];
+    e[2]-=v2.e[2];
     return *this;
 }
 
@@ -101,61 +82,55 @@ inline vec3& vec3::operator/(const float f)
 
 inline vec3& vec3::operator/=(const float f)
 {
-    e[0]/=f;
-    e[1]/=f;
-    e[2]/=f;
-    return *this;
-}
+    float k = 1.0/f;
 
-inline vec3& vec3::operator/(const vec3& v2)
-{
-    e[0]/=v2.x();
-    e[1]/=v2.y();
-    e[2]/=v2.z();
+    e[0]*=k;
+    e[1]*=k;
+    e[2]*=k;
     return *this;
 }
 
 inline vec3& vec3::operator/=(const vec3& v2)
 {
-    e[0]/=v2.x();
-    e[1]/=v2.y();
-    e[2]/=v2.z();
+    e[0]/=v2.e[0];
+    e[1]/=v2.e[1];
+    e[2]/=v2.e[2];
     return *this;
 }
 
 inline vec3 operator+(const vec3& v1, const vec3& v2)
 {
-    return vec3(v1.x() + v2.x(), v1.y() + v2.y(), v1.z() + v2.z());
+    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
 }
 
 inline vec3 operator-(const vec3& v1, const vec3& v2)
 {
-    return vec3(v1.x() - v2.x(), v1.y() - v2.y(), v1.z() - v2.z());
+    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
 }
 
 inline vec3 operator*(const vec3& v1, const vec3& v2)
 {
-    return vec3(v1.x() * v2.x(), v1.y() * v2.y(), v1.z() * v2.z());
+    return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
 }
 
 inline vec3 operator/(const vec3& v1, const vec3& v2)
 {
-    return vec3(v1.x() / v2.x(), v1.y() / v2.y(), v1.z() / v2.z());
+    return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
 }
 
 inline vec3 operator*(float f, const vec3& v)
 {
-    return vec3(v.x() * f,v.y() * f,v.z() * f);
+    return vec3(v.e[0] * f, v.e[1] * f, v.e[2] * f);
 }
 
 inline vec3 operator*(const vec3& v, float f)
 {
-    return vec3(v.x() * f,v.y() * f,v.z() * f);
+    return vec3(v.e[0] * f, v.e[1] * f, v.e[2] * f);
 }
 
 inline vec3 operator/(float f, const vec3& v)
 {
-    return vec3(v.x() / f,v.y() / f,v.z() / f);
+    return vec3(v.e[0] / f,v.e[1] / f, v.e[2] / f);
 }
 
 inline vec3 unit_vector(vec3 v)
@@ -165,5 +140,5 @@ inline vec3 unit_vector(vec3 v)
 
 inline float dot(const vec3& v1, const vec3& v2)
 {
-    return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
 }
