a5c560b9efd629b5bd5cfe913dd0f98acdcaa075
baboulinet_ <vergilotaku@live.fr>
2019-01-25 18:43:08 +0100

    added basic raytracing sphere

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 #include "ray.hpp"
 
 bool hit_sphere(const vec3& center, float radius, const ray& r)
-{
-    // TODO : Implement function hit_sphere
-    return false;
+{    
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discri = b*b - 4*a*c;
+    return (discri > 0);
 }
 
 vec3 color(const ray& r)
 {
+    if (hit_sphere(vec3(0, 0,-1), 0.5, r))
+        return vec3(1.0, 0.0, 0.0);
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5 * (unit_direction.y()+1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
@@ -17,12 +23,12 @@ vec3 color(const ray& r)
 
 int main(int argc, char** argv)
 {
-    const int RENDER_WIDTH = 200;
-    const int RENDER_HEIGHT = 100;
-    vec3 origin = vec3(0.0, 0.0, 0.0);
-    vec3 vertical = vec3(0.0, 2.0, 0.0);
-    vec3 horizontal = vec3(4.0, 0.0, 0.0);
-    vec3 lower_left_corner = vec3(-2.0, -1.0, -1.0);
+    const int RENDER_WIDTH = 800;
+    const int RENDER_HEIGHT = 600;
+    vec3 origin(0.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
     std::ofstream file;
     file.open("output.ppm");
     file << "P3\n" << RENDER_WIDTH << " " << RENDER_HEIGHT << "\n255\n";
