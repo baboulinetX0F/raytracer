962f8f6d37f545fd1720dac23e4bdb75899874fd
baboulinet_ <vergilotaku@live.fr>
2019-01-27 03:05:26 +0100

    normal sphere coloring

--- main.cpp
+++ main.cpp
@@ -22,7 +22,10 @@ float hit_sphere(const vec3& center, float radius, const ray& r) {
 vec3 color(const ray& r) {
     float t = hit_sphere(vec3(0, 0,-1), 0.5, r);
     if (t > 0.0)
-        return vec3(1.0, 0.0, 0.0);
+    {
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+        return 0.5 *vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     t = 0.5 * (unit_direction.y()+1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
