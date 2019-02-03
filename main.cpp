b4c316acf86960b472c71ca8ff9feec6c048081c
baboulinet_ <vergilotaku@live.fr>
2019-02-03 16:38:21 +0100

    diffuse materials

--- main.cpp
+++ main.cpp
@@ -4,20 +4,6 @@
 #include "sphere.hpp"
 #include "hitable_list.hpp"
 
-
-vec3 color(const ray& r, hitable* world) {
-    hit_record hit;
-    if (world->hit(r, 0.0, 10000.0, hit))
-    {
-        vec3 N = hit.normal;
-        return 0.5 *vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-    }
-    // fake sky gradient backgrounc
-    vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5 * (unit_direction.y()+1.0);
-    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-}
-
 float rand_mt19937()
 {
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
@@ -26,11 +12,36 @@ float rand_mt19937()
     return dis(gen);
 }
 
+
+vec3 random_in_unit_sphere()
+{
+    vec3 p;
+    do {
+        p = 2*vec3(rand_mt19937(), rand_mt19937(), rand_mt19937()) - vec3(1,1,1);
+    } while(p.squared_length() >= 1.0);
+    return p;
+}
+
+vec3 color(const ray& r, hitable* world) {
+    hit_record hit;
+    if (world->hit(r, 0.0, 10000.0, hit))
+    {
+        // vec3 N = hit.normal;
+        // return 0.5 *vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+        vec3 target = hit.p + hit.normal + random_in_unit_sphere();
+        return 0.5*color(ray(hit.p, target-hit.p), world);
+    }
+    // fake sky gradient backgrounc
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y()+1.0);
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
 int main(int argc, char** argv)
 {
     const int RENDER_WIDTH = 200;
     const int RENDER_HEIGHT = 100;
-    const int SAMPLE_NUMBER = 100;
+    const int SAMPLE_NUMBER = 10;
     vec3 origin(0.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 horizontal(4.0, 0.0, 0.0);
