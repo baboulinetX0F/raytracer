b4a578df2101b0c364eab33e80417f6ca6bc0fb4
baboulinet_ <vergilotaku@live.fr>
2019-03-02 16:13:16 +0100

    added headerguard for rand/lambertian

--- lambertian.hpp
+++ lambertian.hpp
@@ -1,10 +1,13 @@
+#ifndef LAMBERTIANH
+#define LAMBERTIANH
+
 #include "material.h"
 #include "rand.hpp"
 
 class lambertian : public material {
 public:
     lambertian(const vec3& a) : albedo(a) {}
-    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered)
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
     {
         vec3 target = rec.p + rec.normal + random_in_unit_sphere();
         scattered = ray(rec.p, target-rec.p);
@@ -12,5 +15,6 @@ public:
         return true;
     }
     vec3 albedo;
-
 };
+
+#endif
--- rand.hpp
+++ rand.hpp
@@ -1,8 +1,10 @@
+#ifndef RANDH
+#define RANDH
+
 #include <random>
 #include "vec3.hpp"
 
-float rand_mt19937()
-{
+float rand_mt19937() {
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<> dis(0.0, 1.0);
@@ -17,3 +19,5 @@ vec3 random_in_unit_sphere()
     } while(p.squared_length() >= 1.0);
     return p;
 }
+
+#endif
