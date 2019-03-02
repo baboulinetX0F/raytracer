dbbdedb3438b55575d60a1e3cd5ee6e925dc679e
baboulinet_ <vergilotaku@live.fr>
2019-03-02 15:33:11 +0100

    moved random related functions to rand.hpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <random>
+#include "rand.hpp"
 #include "sphere.hpp"
 #include "hitable_list.hpp"
-
-float rand_mt19937()
-{
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_real_distribution<> dis(0.0, 1.0);
-    return dis(gen);
-}
-
-
-vec3 random_in_unit_sphere()
-{
-    vec3 p;
-    do {
-        p = 2*vec3(rand_mt19937(), rand_mt19937(), rand_mt19937()) - vec3(1,1,1);
-    } while(p.squared_length() >= 1.0);
-    return p;
-}
 
 vec3 color(const ray& r, hitable* world) {
     hit_record hit;
---
+++ rand.hpp
@@ -0,0 +1,20 @@
+#include <random>
+#include "vec3.hpp"
+
+float rand_mt19937()
+{
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+    return dis(gen);
+}
+
+
+vec3 random_in_unit_sphere()
+{
+    vec3 p;
+    do {
+        p = 2*vec3(rand_mt19937(), rand_mt19937(), rand_mt19937()) - vec3(1,1,1);
+    } while(p.squared_length() >= 1.0);
+    return p;
+}
