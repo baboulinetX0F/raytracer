7c2c1f412c7a2a1692b5c7f0c9aba3bc5e7b5f14
Unknown <vergilotaku@live.fr>
2019-03-05 00:13:21 +0100

    fixed infinite recursivity (added max depth)

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "sphere.hpp"
 #include "hitable_list.hpp"
 
-vec3 color(const ray& r, hitable* world) {
+vec3 color(const ray& r, hitable* world, int depth) {
     hit_record hit;
     if (world->hit(r, 0.00001, 10000.0, hit))
     {
@@ -17,9 +17,9 @@ vec3 color(const ray& r, hitable* world) {
         //return 0.5*color(ray(hit.p, target-hit.p), world);
 		vec3 attenuation;
 		ray scattered;
-		if (hit.mat_ptr->scatter(r, hit, attenuation, scattered))
+		if (depth < 50 && hit.mat_ptr->scatter(r, hit, attenuation, scattered))
 		{
-			return attenuation * color(scattered, world);
+			return attenuation * color(scattered, world, depth + 1);
 		}
 		else
 		{
@@ -36,8 +36,8 @@ vec3 color(const ray& r, hitable* world) {
 }
 
 int main(int argc, char** argv)
-{
-    const int RENDER_WIDTH = 200;
+{	
+	const int RENDER_WIDTH = 200;
     const int RENDER_HEIGHT = 100;
     const int SAMPLE_NUMBER = 10;
     vec3 origin(0.0, 0.0, 0.0);
@@ -46,12 +46,13 @@ int main(int argc, char** argv)
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
     std::ofstream file;
     file.open("output.ppm");
-    hitable* list[4];
+    hitable* list[5];
     list[0] = new sphere(vec3(0, 0,-1), 0.5, new lambertian(vec3(0.8, 0.1, 0.0)));	
     list[1] = new sphere(vec3(0, -100.5,-1), 100, new lambertian(vec3(0.0, 0.15, 0.8)));
 	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.0, 0.8, 0.1), 0.2));
 	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-    hitable* world = new hitable_list(list, 4);
+	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
+    hitable* world = new hitable_list(list, 5);
     file << "P3\n" << RENDER_WIDTH << " " << RENDER_HEIGHT << "\n255\n";
     for(int y = RENDER_HEIGHT - 1; y >= 0; y--)
     {
@@ -63,7 +64,7 @@ int main(int argc, char** argv)
                 float u = float(x + rand_mt19937()) / float(RENDER_WIDTH);
                 float v = float(y + rand_mt19937()) / float(RENDER_HEIGHT);
                 ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
             col /= SAMPLE_NUMBER;
             col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
